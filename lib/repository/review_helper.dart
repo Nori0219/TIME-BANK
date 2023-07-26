ef5a79f416a3b05db333ebe3a392b2140a92a112
NorichikaSugii <wawa60379@gmail.com>
2023-07-26 14:43:26 +0900

    :sparkles: レビュー回数をリセット

--- lib/repository/review_helper.dart
+++ lib/repository/review_helper.dart
@@ -5,8 +5,8 @@ import 'package:in_app_review/in_app_review.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ReviewHelper {
-  static const int reviewRequestLimit = 4; //１年間に3回までしかレビュー依頼できない
-  static const String reviewRequestKey = 'ReviewRequestCount';
+  static const int reviewRequestLimit = 3; //１年間に3回までしかレビュー依頼できない
+  static const String reviewRequestKey = 'ReviewRequestCount_20230726';
 
   static Future<void> showReviewDialog(BuildContext context) async {
     final reviewRequestCount = await getReviewRequestCount();
@@ -32,43 +32,6 @@ class ReviewHelper {
         requestAppReview();
       },
     )..show();
-
-    // showDialog(
-    //   context: context,
-    //   builder: (context) {
-    //     return AlertDialog(
-    //       shape: RoundedRectangleBorder(
-    //         borderRadius: BorderRadius.circular(16.r),
-    //       ),
-    //       title: const Text(
-    //         'ご利用ありがとうございます！',
-    //         textAlign: TextAlign.center,
-    //       ),
-    //       titleTextStyle: TextStyle(color: Colors.pink, fontSize: 18.sp),
-    //       content: const Text(
-    //         'アプリを気に入っていただけましたか？レビューでの応援が何よりの励みになります！',
-    //         textAlign: TextAlign.center,
-    //       ),
-    //       actions: [
-    //         TextButton(
-    //           child: const Text('応援する'),
-    //           onPressed: () {
-    //             Navigator.of(context).pop();
-    //             requestAppReview();
-    //           },
-    //         ),
-    //         TextButton(
-    //           child: const Text(
-    //             '応援しない',
-    //           ),
-    //           onPressed: () {
-    //             Navigator.of(context).pop();
-    //           },
-    //         ),
-    //       ],
-    //     );
-    //   },
-    // );
 
     incrementReviewRequestCount();
   }
